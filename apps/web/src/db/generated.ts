cbb9fad475320b71036527bf89a34bdae5a0b39e
yehor-design <yehor.design@gmail.com>
2026-08-11 13:50:11 +0300

    fix(db): preserve generated type output

--- apps/web/src/db/generated.ts
+++ apps/web/src/db/generated.ts
@@ -5,16 +5,11 @@
 
 import type { ColumnType } from "kysely";
 
-export type Generated<T> =
-  T extends ColumnType<infer S, infer I, infer U>
-    ? ColumnType<S, I | undefined, U>
-    : ColumnType<T, T | undefined, T>;
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
 
-export type Int8 = ColumnType<
-  string,
-  bigint | number | string,
-  bigint | number | string
->;
+export type Int8 = ColumnType<string, bigint | number | string, bigint | number | string>;
 
 export type Json = JsonValue;
 
